4636a152784b22a8da6cdc6418461050201bb237
buthed <temaTihonov@yandex.ru>
2022-03-20 22:30:24 +0300

    First commit

---
+++ app/src/main/java/com/example/myhouse/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myhouse.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
